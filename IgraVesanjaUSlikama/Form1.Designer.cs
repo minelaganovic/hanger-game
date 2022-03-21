d0d6063b7fcf1b19a0266b09c8ba1e1de2f0e98e
Minela Ganovic <59210673+minelaganovic@users.noreply.github.com>
2022-03-22 00:02:23 +0100

    Add files via upload

---
+++ IgraVesanjaUSlikama/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace IgraVesanjaUSlikama
+{
+    partial class IgraVesala
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxSlovo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.BackgroundImage = global::IgraVesanjaUSlikama.Properties.Resources.Slika1;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(28, 30);
+            this.panel1.Margin = new System.Windows.Forms.Padding(13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(893, 659);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(960, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "IGRAJ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxSlovo
+            // 
+            this.textBoxSlovo.Location = new System.Drawing.Point(960, 221);
+            this.textBoxSlovo.Name = "textBoxSlovo";
+            this.textBoxSlovo.Size = new System.Drawing.Size(195, 27);
+            this.textBoxSlovo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(942, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 35);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pogodi Slovo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(960, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 31);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(979, 393);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 29);
+            this.label3.TabIndex = 5;
+            // 
+            // IgraVesala
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1183, 721);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSlovo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "IgraVesala";
+            this.Text = "IgraVesala";
+            this.Load += new System.EventHandler(this.IgraVesala_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxSlovo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ IgraVesanjaUSlikama/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IgraVesanjaUSlikama
+{
+    public partial class IgraVesala : Form
+    {
+        string word;
+        string[] solution;
+        char guess;
+        string stringSolution;
+        int badguess;
+        int goodguess;
+
+        public IgraVesala()
+        {
+            InitializeComponent();
+            word = "igrajigru";
+            solution = new string[word.Length];
+            MakeSolutionStrg();
+        }
+        public void MakeSolutionStrg()
+        {
+            string temp = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                solution[i] = "_" + " ";
+                stringSolution = temp + solution[i];
+                temp = stringSolution;
+            }
+            label2.Text = stringSolution;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            bool found = false;
+            guess = textBoxSlovo.Text[0];
+            string temp = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i].CompareTo(guess) == 0)
+                {
+                    solution[i] = guess.ToString();
+                    goodguess++;
+                    if (goodguess == word.Length)
+                    {
+                        label3.Text = "YOU WIN!!";
+                    }
+                    found = true;
+                }
+                stringSolution = temp + solution[i];
+                temp = stringSolution;
+            }
+            if (found == false)
+            {
+                badguess++;
+                if (badguess == 1)
+                {
+                    Graphics g = panel1.CreateGraphics();
+                    Bitmap bitmap1 = new Bitmap(Properties.Resources.Glava);
+                    g.DrawImage(bitmap1, 473, 120, 330, 99);                  
+                }
+                if (badguess == 2)
+                {
+                    Graphics g = panel1.CreateGraphics();
+                    Bitmap bitmap2 = new Bitmap(Properties.Resources.Tijelo);
+                    g.DrawImage(bitmap2, 475, 195, 297, 234);
+                }
+                if (badguess == 3)
+                {
+                    Graphics g = panel1.CreateGraphics();
+                    Bitmap bitmap3 = new Bitmap(Properties.Resources.NogaLEva);
+                    g.DrawImage(bitmap3, 448, 355, 307, 192);
+                }
+                if (badguess == 4)
+                {
+                    Graphics g = panel1.CreateGraphics();
+                    Bitmap bitmap4 = new Bitmap(Properties.Resources.NogaDesna);
+                    g.DrawImage(bitmap4, 627, 348, 127, 195);
+                }
+                if (badguess == 5)
+                {
+                    Graphics g = panel1.CreateGraphics();
+                    Bitmap bitmap5 = new Bitmap(Properties.Resources.RukaLeva);
+                    g.DrawImage(bitmap5, 390, 225, 227, 122);
+                }
+                if (badguess == 6)
+                {
+                    Graphics g= panel1.CreateGraphics();
+                    Bitmap bitmap6 = new Bitmap(Properties.Resources.RukaDesna);
+                    g.DrawImage(bitmap6, 628, 226, 180, 122);
+                    label3.Text = "GAME OVER!";
+                    button1.Visible = false;
+                }
+                }          
+            label2.Text = stringSolution;
+        }
+
+        private void IgraVesala_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
